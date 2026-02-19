83ae088ae3fd88b8e46374eaf28279a991d5d899
VITOR CASAGRANDE GHEDIN <vitor.ghedin@alunos.sc.senac.br>
2026-02-19 20:14:41 -0300

    primeiro commit

---
+++ src/main/java/br/com/senac/teste2/exercicio_api/ExercicioApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.teste2.exercicio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senac/teste2/exercicio_api/ExercicioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.teste2.exercicio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
